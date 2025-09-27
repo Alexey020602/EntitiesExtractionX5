3c512f4e8cdcdd8d12506f228b611502aac0796e
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2025-09-27 14:58:06 +0300

    Переделал main.py чтобы можно было использовать относительный путь к модели

--- Api/python/main.py
+++ Api/python/main.py
@@ -3,10 +3,16 @@ import os
 
 def main():
     model.fine_tuned_model = None
-    path = r"/Users/Alexey/RiderProjects/EntitiesExtractionX5/Api/model"
+    model_path = r"model"
+    script_path = os.path.abspath(__file__)
+    script_directory = os.path.dirname(script_path)
+    parent_directory_of_script = os.path.dirname(script_directory)
+    path = os.path.join(parent_directory_of_script, model_path)
+    print()
+    print(f"Model path {path}")
     if os.path.isdir(path):
         model.initialize(path)
-        print(model.simple_function("Молоко"))
+        print(model.predict("Молоко"))
     else:
         raise ValueError("Directory {path} not exists")
 
